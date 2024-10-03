3170f0893a1c6f470c1823558c37aa58cd06076a
SpaceShaman <spaceshaman@tuta.io>
2024-10-03 12:55:41 +0200

    ✨ (nuxt.config.ts, package.json): add @nuxt/content module to enable content management features

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+  ],
   ssr: false,
   hooks: {
     "prerender:routes"({ routes }) {
