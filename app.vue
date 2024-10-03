b6de0fb87c4553c25bd790e96fae8bd05d283193
SpaceShaman <spaceshaman@tuta.io>
2024-10-03 12:50:33 +0200

    init

---
+++ app.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    style: "user-select: none;",
+  },
+  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+});
+useSeoMeta({
+  title: "Krzysztof Stanisławski - Fullstack Developer",
+  description:
+    "Krzysztof Stanisławski - Fullstack Developer. I'm a fullstack developer with a passion for creating advanced web applications.",
+});
+</script>
+<template>
+  <div class="mx-auto max-w-7xl p-4">
+    <ColorModeButton />
+    <NuxtPage />
+  </div>
+</template>
+<style>
+.dark-mode {
+  @apply bg-dark-bg-0-h text-dark-fg;
+}
+.light-mode {
+  @apply bg-light-bg-0-h text-light-fg;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Space Blog",
+  description:
+    "Blog about programming, web development, AI, machine learning, and more.",
+});
+</script>
+<template></template>
